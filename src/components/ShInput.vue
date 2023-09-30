5e9e1257e473223406d1d6acf1fd68a62ec2b4e2
BarryDong <286084845@qq.com>
2023-09-30 23:25:28 +0800

    调整 ShInput 组件以适应两种样式的输入框 & 首页-顶部搜索框

--- src/components/ShInput.vue
+++ src/components/ShInput.vue
@@ -2,11 +2,14 @@
 import { defineProps, ref, toRefs, watchEffect } from 'vue'
 
 const props = defineProps({
-  modelValue: { type: String, required: true },
+  modelValue: { type: String },
+  size: { type: String, default: 'big' },
+  preIcon: { type: String, default: '' },
   type: { type: String, default: 'text' },
-  placeholder: { type: String, required: true }
+  placeholder: { type: String, required: true },
+  disabled: { type: Boolean, default: false }
 })
-const { modelValue, type, placeholder } = toRefs(props)
+const { modelValue, size, preIcon, type, placeholder, disabled } = toRefs(props)
 
 const inputValue = ref('')
 
@@ -16,24 +19,38 @@ watchEffect(() => {
 </script>
 
 <template>
-  <input
-    v-model="inputValue"
-    :type="type" :placeholder="placeholder"
-    class="shInput"
-    @input="$emit('update:modelValue', inputValue)"
-  >
+  <div :class="['shInput', {'shInput-small': size === 'small'}]">
+    <i v-if="preIcon" class="iconfont">{{ preIcon }}</i>
+    <input
+      v-model="inputValue"
+      :type="type" :placeholder="placeholder" :disabled="disabled"
+      @input="$emit('update:modelValue', inputValue)"
+    >
+  </div>
 </template>
 
 <style scoped lang="scss">
 .shInput {
   box-sizing: border-box;
-  color: #777; font-size: .16rem;
   width: 100%; height: .48rem;
   background: #F9F9F9;
-  border: .01rem solid rgba(0,0,0,0.10);
-  border-radius: .06rem;
-  outline: none;
+  border: .01rem solid rgba(0, 0, 0, 0.10); border-radius: .06rem;
   padding: 0 .16rem;
-  &::placeholder { color: #777; }
+  display: flex; align-items: center;
+  > input {
+    color: #777; font-size: .16rem;
+    flex: 1; align-self: stretch;
+    background: none;
+    border: none; outline: none;
+    padding: 0;
+    &::placeholder { color: #B7B7B7; }
+  }
+}
+
+.shInput-small {
+  height: .32rem;
+  border-radius: .16rem;
+  > i { color: #b7b7b7; margin-right: .08rem; }
+  > input { font-size: .14rem; }
 }
 </style>
--- src/views/home/StaticPart.vue
+++ src/views/home/StaticPart.vue
@@ -1,3 +1,11 @@
+<script setup>
+import ShInput from '@/components/ShInput.vue'
+
+const handleSearch = () => {
+  console.log('11111111111')
+}
+</script>
+
 <template>
 
   <div class="address">
@@ -6,13 +14,12 @@
     <i class="iconfont">&#xe60b;</i>
   </div>
 
-</template>
+  <div class="search">
+    <ShInput size="small" pre-icon="&#xe62d;" disabled placeholder="山姆会员商店优惠商品" />
+    <div class="search_mask" @click.capture="handleSearch"></div>
+  </div>
 
-<script>
-export default {
-  name: 'StaticPart'
-}
-</script>
+</template>
 
 <style scoped lang="scss">
 @import "../../style/mixins.scss";
@@ -27,6 +34,15 @@ export default {
   &_text {
     margin: 0 auto 0 .08rem;
     @include ellipsis;
+  }
+}
+
+.search {
+  border: 1px solid red;
+  margin-top: .17rem;
+  position: relative;
+  &_mask {
+    position: absolute; left: 0; top: 0; right: 0; bottom: 0;
   }
 }
 </style>
